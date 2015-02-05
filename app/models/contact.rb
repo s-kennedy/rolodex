85565d9f92b59ff7ecbc1f0f29f2431028f89a74
Sharon <sharon.peishan.kennedy@gmail.com>
2015-02-05 20:17:24 +0100

    next and previous methods to skip empty records

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -4,6 +4,23 @@ class Contact < ActiveRecord::Base
 	validates :email, presence: true, uniqueness: true
 	validates :phone, presence: true
 
+	def next
+		next_contact = Contact.where("id > ?", self.id).limit(1).first
+		if next_contact == nil
+			Contact.first.id
+		else 
+			next_contact.id
+		end
+	end
+
+	def previous
+		previous_contact = Contact.where("id < ?", self.id).reverse_order.limit(1).first
+		if previous_contact == nil
+			Contact.last.id
+		else 
+			previous_contact.id
+		end
+	end
 
 end
 
